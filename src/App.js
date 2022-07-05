b9b9cb74aea54afc19a27ee0d277354abb7aba52
Pritom <pritamparashar8@gmail.com>
2022-07-05 21:38:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { QRCodeCanvas } from 'qrcode.react';
+import { useState } from 'react';
 
 function App() {
+  const [text, setText] = useState("")
+  
+  function getQRcode(e) {
+
+    setText(e.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App antialiased h-screen flex flex-col justify-center bg-gradient-to-r from-rose-500 via-violet-800 to-fuchsia-800 items-center text-center">
+      <h1 className="text-6xl font-bold mb-20 text-white ">QR Code <br/> Generator</h1>
+      <div className='shadow-xl'>
+        <QRCodeCanvas size={300} value={text} />
+      </div>
+      <div>
+        <input type='text' placeholder='Enter your text here' className=' mt-20 px-9 py-3 outline-none shadow-2xl text-xl rounded-lg font-medium' value={ text } onChange={(e)=>{getQRcode(e)}} />
+      </div>
+      <h3 className='mt-20 font-semibold text-white'>Made with ❤️ by <a target="_blank" rel="noreferrer" href='https://twitter.com/itsprtm'>Prtm</a></h3>
     </div>
   );
 }
